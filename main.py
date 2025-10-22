e4cb8138875ac01c006538853e2a1ee79832e681
castilhoserafim <wesleycserafim@gmail.com>
2025-10-22 01:55:49 -0300

    Reprovados e aprovados
    Coloquei o IF e ELSE na brincadeira

--- main.py
+++ main.py
@@ -11,4 +11,7 @@ for x in range(5):
 for n in notas:
     codigo_aluno = n[0]
     nota = n[1]
-    print("O RM", codigo_aluno, "tirou a nota", nota)
+    if nota >= 6:
+        print("O RM", codigo_aluno, "tirou a nota", nota)
+    else:
+        print("O RM", codigo_aluno, "reprovou com:", nota)
