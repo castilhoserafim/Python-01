34a5e523269bddbabc21623d018dc33fad64483b
castilhoserafim <wesleycserafim@gmail.com>
2025-10-22 01:30:59 -0300

    Salários (Input)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-idade = int( input ("Qual a sua idade? ") )
+salario = float(input("Informe o salário: "))
 
-if idade >= 18:
-    print("Permitido!")
+if salario <= 3000:
+    print("programador júnior")
+elif salario > 3000 and salario <= 6000:
+    print("programador pleno")
+elif salario > 6000 and salario <= 15000:
+    print("programador senior")
 else:
-    print("Proibido!")
+    print("diretor")
