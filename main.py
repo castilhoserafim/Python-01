194a983a528c9cad6c93051dd4a85fba8b8281ba
castilhoserafim <wesleycserafim@gmail.com>
2025-10-22 02:23:35 -0300

    Jogo das direções

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-def minha_funcao(valor1, valor2):
-    return valor1 + valor2
+import os
+
+player = { "nome": "Python", "x": 0, "y": 0 }
+
+def andar(direcao):
+    if direcao == "d":
+        player['x'] += 1
+    elif direcao == "a":
+        player['x'] -= 1
+    elif direcao == "w":
+        player['y'] -= 1
+    elif direcao == "s":
+        player['y'] += 1
 
 
 while True:
-    valor1 = int(input("Valor1: "))
-    valor2 = int(input("Valor2: "))
+    os.system('clear')
 
-    resposta = minha_funcao(valor1, valor2)
-    print(valor1, "+", valor2, "=", resposta)
+    print("--------------------")
+    for y in range(5):
+        print("\n")
+        for x in range(10):
+            if player['x'] == x and player['y'] == y:
+                print("🚶", end="")
+            else:
+                print("🟩", end="")
+    print("--------------------")
+
+    direcao = input("Próxima direção (w,s,d,a): ")
+    andar(direcao)
