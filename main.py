ec42ac5b0ed5c1a3bdb45298ee31cad16c0db2fb
castilhoserafim <wesleycserafim@gmail.com>
2025-10-22 01:46:09 -0300

    Notas

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-salario = float(input("Informe o salário: "))
+notas = []
 
-if salario <= 3000:
-    print("programador júnior")
-elif salario > 3000 and salario <= 6000:
-    print("programador pleno")
-elif salario > 6000 and salario <= 15000:
-    print("programador senior")
-else:
-    print("diretor")
+for x in range(5):
+    codigo_aluno = input("RM: ")
+    nota = float(input("Nota: "))
+    resultado = [codigo_aluno, nota]
+    notas.append(resultado)
+
+    print( "quantidade de notas", len(notas) )
+
+for n in notas:
+    codigo_aluno = n[0]
+    nota = n[1]
+    print("O RM", codigo_aluno, "tirou a nota", nota)
