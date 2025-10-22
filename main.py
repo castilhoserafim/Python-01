f1c6f61d29787c489723cfa56023a7c6a237e014
castilhoserafim <wesleycserafim@gmail.com>
2025-10-22 02:15:17 -0300

    Funções

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-import os
+def minha_funcao(valor1, valor2):
+    return valor1 + valor2
 
-mensagens = []
-
-nome = input("Nome: ")
 
 while True:
+    valor1 = int(input("Valor1: "))
+    valor2 = int(input("Valor2: "))
 
-    # Limpando Terminal
-    os.system('cls')
-
-    if len(mensagens) > 0:
-        for m in mensagens:
-            print(m['nome'], "-", m['texto'])
-
-    print("_________________")
-
-    # obtendo texto
-    texto = input("mensagem: ")
-    if texto == "fim":
-        break
-
-    # adicionando mensagem na lista
-    mensagens.append({
-        "nome": nome,
-        "texto": texto
-    })
+    resposta = minha_funcao(valor1, valor2)
+    print(valor1, "+", valor2, "=", resposta)
